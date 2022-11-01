ef952eecfd4ed4420080743b0d86fbe173d3bc4d
zyuandong <de1719@163.com>
2022-11-01 13:46:21 +0800

    fix: 引入 element-Plus 样式文件

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,17 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import { createPinia } from 'pinia';
 import '@/styles/index.scss';
 import router from './router';
-
 
 import '@/mock/config';
 
 const app = createApp(App);
 const pinia = createPinia();
 
-app.use(ElementPlus, { size: 'small', zIndex: 3000 });
+app.use(ElementPlus, { zIndex: 3000 });
 app.use(pinia);
 app.use(router);
 
