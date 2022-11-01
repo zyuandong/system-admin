bd2ccaf86cdfb135b8a84d9542f68424abb5b607
77 <1825642753@qq.com>
2022-11-01 11:44:26 +0800

    feat:用户管理

--- src/mock/config.ts
+++ src/mock/config.ts
@@ -6,11 +6,18 @@ const mock = new mockAdapter(axios);
 
 // Mock any GET request to /users
 // arguments for reply are (status, data, headers)
+interface user  {
+  id:number,
+  name:string
+  address:string
+}
+
+let users: user[]=[ 
+  { id: 1, name: 'John Smith',address:'北京' },
+  { id: 2, name: 'Tom' ,address:'上海' },
+];
 mock.onGet('/users').reply(200, {
-  users: [
-    { id: 1, name: 'John Smith' },
-    { id: 2, name: 'Tom' },
-  ],
+  users
 });
 
 mock.onGet('/users', { params: { searchText: 'John' } }).reply(200, {
@@ -19,8 +26,60 @@ mock.onGet('/users', { params: { searchText: 'John' } }).reply(200, {
 
 axios.get('/users').then(function (response) {
   console.log(response.data);
+  users = response.data.users;
 });
 
 axios.get('/users', { params: { searchText: 'John' } }).then(function (res) {
   console.log(res.data);
+});
+
+mock.onGet('/user/edit').reply(config => {
+  const { id, name,address } = config.params;
+  users.some(u => {
+    if (u.id === id) {
+      u.name = name;
+      u.address=address;
+      return true;
+    }
+  });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([200, {
+        code: 200,
+        msg: '编辑成功'
+      }]);
+    }, 500);
+  });
+});
+
+mock.onGet('/user/add').reply(config => {
+  const { id,name,address } = config.params;
+  users.push({
+    id:id,
+    name: name,
+    address:address
+  });
+  console.log(users);
+  return new Promise((resolve ) => {
+    setTimeout(() => {
+      resolve([200, {
+        code: 200,
+        msg: '新增成功'
+      }]);
+    }, 500);
+  });
+});
+
+mock.onGet('/user/remove').reply(config => {
+  const { id } = config.params;
+  users = users.filter(u => u.id !== id);
+  console.log(users);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([200, {
+        code: 200,
+        msg: '删除成功'
+      }]);
+    }, 500);
+  });
 });
---
+++ src/views/user/components/userForm.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <el-dialog
+      :model-value="visible"
+      title="用户表单"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <el-form :model="tableForm" label-width="60px">
+        <el-form-item label="姓名">
+          <el-input v-model="tableForm.name" />
+        </el-form-item>
+        <el-form-item label="地址">
+          <el-input v-model="tableForm.address" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="handleClose">取消</el-button>
+          <el-button type="primary" @click="handleSubmit"> 确定 </el-button>
+        </span>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+<script lang="ts">
+import axios from 'axios';
+import { computed, defineComponent } from 'vue';
+export default defineComponent({
+  emits: ['close'],
+  props: {
+    visible: Boolean,
+    form: {
+      type: Object,
+    },
+    getTableData: { type: Function, required: true },
+    isEdit: Boolean,
+  },
+  setup(props, { emit, expose }) {
+    const dialogVisible = computed<boolean>(() => {
+      return props.visible;
+    });
+    const tableForm = computed<any>(() => {
+      return props.form;
+    });
+
+    // const userForm = reactive({
+    //   tableForm: props.form,
+    // });
+    const handleClose = () => {
+      emit('close', false);
+    };
+    const handleSubmit = () => {
+      if (props.isEdit) {
+        axios.get('/user/edit', { params: props.form }).then(() => {
+          emit('close', false);
+          props.getTableData();
+        });
+      } else {
+        axios
+          .get('/user/add', { params: { id: 3, ...props.form } })
+          .then(() => {
+            emit('close', false);
+            props.getTableData();
+          });
+      }
+    };
+    expose({ handleClose });
+    return {
+      dialogVisible,
+      handleClose,
+      // ...toRefs(userForm),
+      handleSubmit,
+      tableForm,
+    };
+  },
+});
+</script>
