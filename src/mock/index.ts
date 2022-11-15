b6e8dcf0fd1e81f304a4bfd478ed52fb62ae1ab5
zyuandong <de1719@163.com>
2022-11-15 15:57:33 +0800

    refactor: mock user

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,5 +1,20 @@
+import mock from './config';
 import user from './user';
 import goods from './goods';
 
-user();
+/**
+ * user
+ */
+
+mock.onGet('/users').reply(200, user.userList());
+mock
+  .onGet('/users', { params: { searchText: 'John' } })
+  .reply(200, user.userList('John'));
+mock.onGet('/user/edit').reply(200);
+mock.onGet('/user/add').reply(200);
+mock.onGet('/user/remove').reply(200);
+
+/**
+ * goods
+ */
 goods();
